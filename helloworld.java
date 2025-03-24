0bce0ca42d66b8cf4d5dec7e40cf6cefa68daafd
HareeshaBadepalli <140062639+HareeshaBadepalli@users.noreply.github.com>
2025-03-24 16:32:39 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class example{
     public static void main(String []args){
-        System.out.println("hello world");
+        System.out.println("Hello world");
         System.out.println("welcome to java program");
     }
 }
